d147bb75c630fe86a2346a5612e7dc3de1cb77ff
carlostroisdes <117764678+troisdes@users.noreply.github.com>
2024-11-18 23:49:28 +0100

    Add functions to fetch and display categories and works from the API with error handling

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,44 @@
+async function getCategories () {
+    try
+    {
+        const getCategories = await fetch("http://localhost:5678/api/categories");
+        if (!getCategories.ok)
+        {
+            throw new Error('Erreur réseau : ' + getCategories.status);
+        }
+        const getCategoriesData = await getCategories.json();
+        console.log('Données récupérées :', getCategoriesData);
+    } catch (error)
+    {
+        console.error('Il y a eu un problème avec la requête fetch :', error);
+    }
+}
+
 async function getWorks () {
     try
     {
         // Effectue une requête HTTP GET vers l'URL spécifiée
-        const response = await fetch("http://localhost:5678/api/works");
+        const getWorks = await fetch("http://localhost:5678/api/works");
 
         // Vérifie si la réponse est correcte (status code 200-299)
-        if (!response.ok)
+        if (!getWorks.ok)
         {
             // Si la réponse n'est pas correcte, lance une erreur avec le status de la réponse
-            throw new Error('Erreur réseau : ' + response.status);
+            throw new Error('Erreur réseau : ' + getWorks.status);
         }
 
         // Convertit la réponse en format JSON
-        const data = await response.json();
-        console.log('Données récupérées :', data);
+        const getWorksData = await getWorks.json();
+        console.log('Données récupérées :', getWorksData);
         // Mettre à jour l'interface avec les données
 
         // Sélectionne l'élément avec la classe "gallery" dans le DOM
-        const gallery = document.querySelector(".gallery");
+        const getWorksGallery = document.querySelector(".gallery");
         // Vide le contenu actuel de la galerie
-        gallery.innerHTML = "";
+        getWorksGallery.innerHTML = "";
 
         // Parcourt chaque élément de données récupérées
-        data.forEach(work => {
+        getWorksData.forEach(work => {
             // Crée un élément <figure>
             const figure = document.createElement("figure");
             // Crée un élément <img> et définit ses attributs src et alt
@@ -37,7 +53,7 @@ async function getWorks () {
             figure.appendChild(img);
             figure.appendChild(figCaption);
             // Ajoute la figure à la galerie
-            gallery.appendChild(figure);
+            getWorksGallery.appendChild(figure);
         });
     } catch (error)
     {
